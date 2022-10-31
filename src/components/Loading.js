63598c9576167aa83cbfd37ab16e917309e2f4c0
Haristr <haristr5235@gmail.com>
2022-10-31 12:20:23 +0530

    adding features

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,8 +3,8 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="grid h-75 place-items-center">
-      <div className="flex item-center">
-        <div class="border-t-transparent border-solid animate-spin rounded-full border-indigo-600 border-4 h-8 w-8 mr-2"></div>
+      <div className="flex items-center">
+        <div className="border-t-transparent border-solid animate-spin rounded-full border-indigo-600 border-4 h-6 w-6 mr-2"></div>
         <h1 className="font-medium text-xl">Loading...</h1>
       </div>
     </div>
